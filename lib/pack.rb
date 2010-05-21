a8569ba9d9ef8683f3a31b88157443aa18bee3b0
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-05-21 14:19:48 -0700

    Mostly overhauled pack. The new load file syntax makes it much more flexible!

--- lib/pack.rb
+++ lib/pack.rb
@@ -1,91 +1,113 @@
+# Pack
+#   by Andrew Smith
+
 class Pack
   DIR   = File.dirname(__FILE__)
   TYPES = %w(js css)
   
-  def self.js(location, destination)
-    pack :js, location, destination
+  def self.pack(file)
+    self.parse_load_file(file).each do |collection|
+      
+      content = read_files(collection[:files])
+      
+      verb = collection[:mode] == 'compress' ? 'Compressing' : 'Combining'
+      puts "#{verb} #{collection[:target_file_short]}..."
+      puts '  - ' + collection[:short_files].join("\n  - ")
+      
+      if collection[:mode] == 'compress'
+        old_content = content
+        
+        if collection[:type] == 'js'
+          content = compress_javascript collection[:files]
+        else
+          content = minify_css(content)
+        end
+        
+        comparison = compare_strings old_content, content
+        bytes = comparison[:difference] == 1 ? 'byte' : 'bytes'
+        puts "#{comparison[:difference]} #{bytes} of #{comparison[:first]} saved (#{comparison[:percent_difference]}%)"
+      end
+      
+      # Write file
+      File.open(collection[:target_file], 'w') {|file| file.write content}
+    end
   end
-  
-  def self.css(location, destination)
-    pack :css, location, destination
-  end
-  
+
 private
   
-  def self.pack(type, location, destination)
-    return false unless TYPES.include?(type.to_s)
-    
-    location += '/' if location.split('').last != '/'
-    
-    begin
-      load_file = File.new(location + 'load.txt')
-      files     = parse_load_file(load_file, type)
-      combined  = read_files(files)
-    rescue
-      raise Exception, "Could not find #{location}load.txt"
-    end
-    
-    case type
-    when :css
-      puts "Compressing CSS"
-      compressed = minify_css(combined)
-    when :js
-      puts "Compressing Javascript"
-      compressed = compress_javascript(files)
-    end
-    
-    # Print out comparison table
-    compare_strings :original => combined, :compressed => compressed
-    
-    # Save file
-    dirs = destination.split('/')
-    dirs.pop # Remove the last file
-    
-    # Make directories if they don't exist
-    dirs.each_with_index do |dir, i|
-      path = '/' + dirs.slice(0, i + 1).join('/')
-      unless File.directory? path
-        puts "Creating #{dir} directory"
-        Dir.mkdir(path) 
-      end
-    end
-    
-    puts "Saving to #{destination}"
-    File.open(destination, 'w') {|file| file.write compressed}
-  end
-  
-  def self.parse_load_file(file, type)
-    extension = Regexp.new(".#{type}")
-    array  = []
-    output = []
+  def self.parse_load_file(path)
+    file   = File.new(path)
     dir    = File.dirname(file.path) + '/'
+    result = []
+    extension = nil
+    current_file = nil
+    current_collection = nil
+    current_file_array = []
+    
     file.read.split("\n").each do |line|
-      # Skip comments
-      next if line =~ /^#|^$/
-      new_file = nil
+      # Skip comments and empty lines, remove whitespace
+      line.strip!
+      next if line.split('').first == '#' || line == ''
       
-      # Loop through directory if path ends in *
-      if line =~ /\*$/
-        path = line.gsub!(/\*$/, '');
-        Dir.new(dir + path).each do |f|
-          # Skip invisible files
-          if f =~ extension and f.split('').first != '.'
-            output << path + f
-            array  << dir + path + f
+      # Parse "@" statements
+      if line =~ /^@/
+        matches  = line.match /@([^ ]+) (.*)/
+        mode     = matches[1].to_s
+        new_path = matches[2].to_s
+        
+        # Make sure a mode and a new_path is supplied.
+        # Otherwise, ignore this line.
+        
+        unless mode.empty? or new_path.empty?
+          # The type is the "js" in "something.js"
+          type = get_file_type(new_path)
+          extension = Regexp.new ".#{type}$"
+          
+          raise Exception, 'Invalid type' unless TYPES.include? type
+          
+          current_file = ensure_absolute_path(new_path, dir)
+          
+          current_collection = {
+            :mode              => mode,
+            :files             => [],
+            :type              => type,
+            :target_file       => current_file,
+            :target_file_short => new_path,
+            :short_files       => []
+          }
+          
+          result << current_collection
+          current_file_array = current_collection[:files]
+        end
+      elsif current_file
+        # Loop through directory if path ends in *
+        if line =~ /\*$/
+          path = ensure_absolute_path(line.gsub!(/\*$/, ''), dir)
+          Dir.new(path).each do |f|
+            # Skip invisible files or files with the wrong type
+            if f =~ extension and f.split('').first != '.'
+            current_collection[:short_files] << path + f
+              current_file_array << ensure_absolute_path(path + f, dir)
+            end
+          end
+        else
+          if line =~ extension
+            current_collection[:short_files] << line
+            current_file_array << ensure_absolute_path(line, dir)
           end
         end
-      # Add file normally
-      else
-        if line =~ extension
-          output << line
-          array  << dir + line
-        end
       end
+      
+      # Make sure the file we write to never gets read
+      current_file_array.delete(current_file)
     end
     
     puts 'Reading ' + file.path
-    puts output.collect {|line| "  - #{line}"}.join("\n")
-    array
+    result
+  end
+  
+  def self.ensure_absolute_path(path, absolute_prefix)
+    return path[0, 1] == '/' ? path : absolute_prefix + path
   end
   
   def self.read_files(array)
@@ -94,6 +116,15 @@ private
       contents += File.read(file)
     end
     contents
+  end
+  
+  def self.get_file_type(filename)
+    filename.match(/\.([^\.]+$)/)[1]
+  end
+  
+  def self.compress_javascript(files)
+    files = files.collect { |file| "--js=#{file}" }.join(' ')
+    `java -jar #{DIR}/java/compiler.jar --warning_level=QUIET #{files}`
   end
   
   def self.minify_css(css)
@@ -106,48 +137,31 @@ private
 
     # Remove semicolons next to end brackets
     css.gsub(/;\}/, '}')
+    
+    # Remove empty rules
+    css.gsub(/[^\{\}]+\{\}/, '')
   end
   
-  def self.compress_javascript(files)
-    files = files.collect { |file| "--js=#{file}" }.join(' ')
-    `java -jar #{DIR}/java/compiler.jar --warning_level=QUIET #{files}`
-  end
-  
-  def self.compare_strings(hash)
-    summary = 'Difference'
-    
-    # Find longest name
-    length = summary.length
-    hash.each_key do |name|
-      name = name.to_s
-      length = name.length if name.length > length
-    end
-    # Pad length for spacing
-    length += 4
-    
-    bytes_array = []
-    hash.each_pair do |name, value|
-      # Count bytes
+  def self.compare_strings(first, second)
+    [first, second].each_with_index do |string, index|
       bytes = 0
-      value.each_byte {|b| bytes += 1}
-      hash[name] = bytes
-      bytes_array << bytes
+      string.each_byte {|b| bytes += 1}
+      
+      if index == 0
+        first = bytes
+      else
+        second = bytes
+      end
     end
     
-    # Sort by number of bytes descending
-    stats = []
-    bytes_array.sort!.reverse!.each do |bytes|
-      name = hash.invert[bytes].to_s.capitalize.ljust(length)
-      stats << name + bytes.to_s
-    end
+    percent_difference = ((first - second).to_f / first) * 100
+    percent_difference = (percent_difference * 100).round / 100.to_f
     
-    # Find percentage
-    first = bytes_array.first
-    last  = bytes_array.last
-    percentage = 100 - ((last / first.to_f) * 100)
-    percentage = (percentage * 100).round / 100.to_f
-    stats << summary.ljust(length) + "#{first - last} (#{percentage}%)"
-    
-    stats.each {|stat| puts "  - #{stat}"}
+    return {
+      :first      => first,
+      :second     => second,
+      :difference => first - second,
+      :percent_difference => percent_difference
+    }
   end
 end
